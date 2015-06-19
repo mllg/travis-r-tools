d98d053be94381dfa13d65bc0cddd0c6de7e97b9
Michel <michellang@gmail.com>
2015-06-19 19:57:46 +0200

    be more verbose

--- update-packages.r
+++ update-packages.r
@@ -3,14 +3,20 @@
 # packages in the system lib by 'overloading' these packages
 # with an installation to the user lib
 tryCatch({
+  cat("Searching for outdated packages ...\n", file = stdout())
+  getPkgs = function(x) if (is.null(x)) character(0L) else unname(x[, "Package"])
   user.lib = .libPaths()[1L]
   sys.lib = .libPaths()[2L]
-  old = unname(old.packages(lib = sys.lib)[, "Package"])
-  new = unname(installed.packages(lib = user.lib)[, "Package"])
+  old = getPkgs(old.packages(lib = sys.lib))
+  new = getPkgs(installed.packages(lib = user.lib))
   req = old[match(old, new, nomatch = 0L) == 0L]
 
   if (length(req)) {
+    cat(sprintf("Updating %i binary packages: %s\n", length(req), paste(req, collapse = ", ")), file = stdout())
     options(repos = "http://cran.rstudio.com")
     install.packages(req)
+  } else {
+    message("All binary packages up-to-date.\n", file = stdout())
   }
-}, error = function(e) { message(e); q(status = 1, save = "no") })
+}, error = function(e) { cat(e, "\n", file = stderr()); flush(stderr()); q(status = 1L, save = "no") })
+q(status = 0L, save = "no")
